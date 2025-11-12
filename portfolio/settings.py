2c5a7061f1813b35b52c2a5870dc3eb9a7e370f4
Lwazi Gumede <Lwazi.Gumede@transnet.net>
2025-11-12 09:01:13 +0200

    Add PythonAnywhere domain to ALLOWED_HOSTS

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-dev-key-change-this-in-production-12345'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'stephusband.pythonanywhere.com']
 
 
 # Application definition
